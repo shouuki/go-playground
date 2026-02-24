c98efb4d188aa225ae53c3e6ea77fecfdfe638af
shouuki <2054939673@qq.com>
2026-02-24 22:43:28 +0800

    refactor(utility): let MustDo and MustGet wrap original error when panic

--- utility/must.go
+++ utility/must.go
@@ -6,27 +6,27 @@ import (
 )
 
 // errMsgFormat is the format of the error message.
-const errMsgFormat = "unexpected error: %s\n\tat %s(%s:%d)"
+const errMsgFormat = "unexpected error: %w [at %s(%s:%d)]"
 
 // MustDo panics if err is not nil.
 func MustDo(err error) {
 	if err != nil {
-		pc, file, line, ok := runtime.Caller(1)
-		if !ok {
+		if pc, file, line, ok := runtime.Caller(1); !ok {
 			panic(err)
+		} else {
+			panic(fmt.Errorf(errMsgFormat, err, runtime.FuncForPC(pc).Name(), file, line))
 		}
-		panic(fmt.Sprintf(errMsgFormat, err.Error(), runtime.FuncForPC(pc).Name(), file, line))
 	}
 }
 
 // MustGet panics if err is not nil or else returns the given value.
 func MustGet[T any](v T, err error) T {
 	if err != nil {
-		pc, file, line, ok := runtime.Caller(1)
-		if !ok {
+		if pc, file, line, ok := runtime.Caller(1); !ok {
 			panic(err)
+		} else {
+			panic(fmt.Errorf(errMsgFormat, err, runtime.FuncForPC(pc).Name(), file, line))
 		}
-		panic(fmt.Sprintf(errMsgFormat, err.Error(), runtime.FuncForPC(pc).Name(), file, line))
 	}
 	return v
 }
